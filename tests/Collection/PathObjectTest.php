9abcfec74532174e8921de3ecb897502c65bf5b8
Shigeki Tanaka <dev@shigeki.tokyo>
2019-06-21 14:17:59 +0900

    add test

---
+++ tests/Collection/PathObjectTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace Chanshige\Backlog\Collection;
+
+use Chanshige\Backlog\BaseTestCase;
+use Closure;
+
+/**
+ * Class PathObjectTest
+ *
+ * @package Chanshige\Backlog\Collection
+ */
+final class PathObjectTest extends BaseTestCase
+{
+    /**
+     * Protectedメソッドのテスト
+     */
+    public function testCreatePath()
+    {
+        Closure::bind(function () {
+            // http://www.rfc-editor.org/rfc/rfc3092.txt
+            $path = (new PathObject(['meta_syntactic_variable']))
+                ->foo(1)
+                ->bar(2)
+                ->baz()
+                ->qux(3)
+                ->quux(4)
+                ->corge();
+
+            $expected = 'meta_syntactic_variable/foo/1/bar/2/baz/qux/3/quux/4/corge';
+            $this->assertEquals($expected, $path->createPath());
+        }, $this, PathObject::class)();
+    }
+}
