210e76a32aa157bd16ba970253f7e7e2be372c92
Shigeki Tanaka <dev@shigeki.tokyo>
2019-06-27 15:26:24 +0900

    add test case

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -1,7 +1,9 @@
 <?php
 namespace Chanshige\Backlog;
 
+use Chanshige\Backlog\Collection\ArrayList;
 use Chanshige\Backlog\Fake\Common;
+use Chanshige\Backlog\Interfaces\RequestInterface;
 use Chanshige\Backlog\Provider\ResourceProvider;
 use Symfony\Contracts\HttpClient\ResponseInterface;
 
@@ -33,7 +35,23 @@ final class ApiTest extends BaseTestCase
     {
         $space = $this->backlog->space()->get();
         $this->assertInstanceOf(ResponseInterface::class, $space);
-        $this->assertEquals('GET', $space->getInfo('http_method'));
-        $this->assertEquals('https://test.baclog.example/api/v2/space?apiKey=api-key-fake', $space->getInfo('url'));
+        $this->assertEquals(RequestInterface::GET, $space->getInfo('http_method'));
+
+        $expected = 'https://test.baclog.example/api/v2/space?apiKey=api-key-fake';
+        $this->assertEquals($expected, $space->getInfo('url'));
+    }
+
+    public function testGetRequestWithParameter()
+    {
+        $param = (new ArrayList)
+            ->set('key1', 'foo')
+            ->set('key2', 'bar')
+            ->set('key3', 'baz');
+
+        $space = $this->backlog->issues(123456)->withParameters($param)->get();
+        $this->assertEquals(RequestInterface::GET, $space->getInfo('http_method'));
+
+        $expected = 'https://test.baclog.example/api/v2/issues/123456?apiKey=api-key-fake&key1=foo&key2=bar&key3=baz';
+        $this->assertEquals($expected, $space->getInfo('url'));
     }
 }
